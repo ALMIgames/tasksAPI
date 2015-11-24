5b303a01e0481823e395871bdf7ebc4c6c3f40b4
albert <albertxerta93@hotmail.com>
2015-11-24 21:16:25 +0100

    seeders created with faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Task;
+use App\Tag;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +17,33 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $faker = Faker\Factory::create();
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
 
         Model::reguard();
+    }
+
+    public function seedTasks($faker)
+    {
+        foreach (range(0,100) as $number){
+            $task = new Task();
+
+            $task->name = $faker->sentence;
+            $task->done = $faker->boolean;
+            $task->priority = $faker->randomDigit;
+
+            $task-> save();
+        }
+    }
+
+    public function seedTags($faker)
+    {
+        foreach (range(0,100) as $number){
+            $tag = new Tag();
+
+            $tag->name = $faker->word;
+            $tag-> save();
+        }
     }
 }
